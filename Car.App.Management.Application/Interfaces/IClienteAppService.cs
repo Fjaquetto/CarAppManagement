1cda9df1b464cdd8860731d391fe3a936a63c44a
Felipe Silva Jaquetto <felp_jaquetto@hotmail.com>
2020-07-13 01:39:47 -0300

    controller

--- Car.App.Management.Application/Interfaces/IClienteAppService.cs
+++ Car.App.Management.Application/Interfaces/IClienteAppService.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Car.App.Management.Application.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Car.App.Management.Application.Interfaces
 {
     public interface IClienteAppService : IDisposable
     {
+        Task<bool> Adicionar(ClienteViewModel carroViewModel);
+        Task<bool> Atualizar(ClienteViewModel carro);
+        Task<bool> Remover(int id);
+        Task<List<ClienteViewModel>> ObterTodos();
     }
 }
---
+++ Car.App.Management.Services.Api/Controllers/ClienteController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Car.App.Management.Application.Interfaces;
+using Car.App.Management.Application.ViewModels;
+using Car.App.Management.Domain.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car.App.Management.Services.Api.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/clientes")]
+    public class ClienteController : ApiController
+    {
+        private readonly IClienteAppService _clienteAppService;
+
+        public ClienteController(IClienteAppService clienteAppService,
+                                IUser user) : base(user)
+        {
+            _clienteAppService = clienteAppService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(ClienteViewModel clienteViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new
+                {
+                    success = false,
+                    errors = ModelState
+                });
+            }
+
+            await _clienteAppService.Adicionar(clienteViewModel);
+
+            return Ok(clienteViewModel);
+        }
+
+        [HttpGet]
+        public async Task<List<ClienteViewModel>> Get()
+        {
+            return await _clienteAppService.ObterTodos();
+        }
+    }
+}

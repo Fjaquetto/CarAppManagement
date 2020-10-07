bfc3dc37abb65235638bea8175a4eb50a2512642
unknown <felp_jaquetto@hotmail.com>
2020-10-07 01:18:01 -0300

    nome de usuário na tela principal.

--- CarAppManagementPresentation/src/app/home/home.component.ts
+++ CarAppManagementPresentation/src/app/home/home.component.ts
@@ -6,11 +6,14 @@ import { LoginComponent } from '../login/login.component';
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+  nomeUsuario: object;
 
   constructor(private login: LoginComponent) {  }
 
   ngOnInit(): void {
     this.login.verificaUsuarioLogado();
+
+    this.nomeUsuario = JSON.parse(localStorage.getItem("user-token")).name;
   }
 
 }
